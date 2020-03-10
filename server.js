d7f9b1c560082962c4f2cf675b491fb2de7f38da
Asokumar Gurusamy <agurusamy@paypal.com>
2020-03-09 23:24:08 -0700

    static asset

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const morgon = require('morgan');
@@ -20,6 +21,11 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 app.use("/api/v1/transactions", transactions);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")));
+}
 
 const PORT = process.env.PORT || 5000;
 
